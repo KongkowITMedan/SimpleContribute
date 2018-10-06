5833740fa4dbea6ecb3858d17ff7b8418f303ecf
In Sin <bagongkia@gmail.com>
2018-10-07 00:24:55 +0700

    add comment: how to compile cobol online

--- scripts/bagongkia_hello.cob
+++ scripts/bagongkia_hello.cob
@@ -1,5 +1,7 @@
+*> Try to run the code at https://www.tutorialspoint.com/compile_cobol_online.php
 IDENTIFICATION DIVISION.
 PROGRAM-ID. HELLO-WORLD.
 PROCEDURE DIVISION.
+AUTHOR. BAGONGKIA.
 DISPLAY 'Hello, world'.
 STOP RUN.
